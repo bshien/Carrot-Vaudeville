b128a37ac85a04e254684ca30a5378484d3d1974
Brandon Shien <brandon@macbook-pro-7.hsd1.ca.comcast.net>
2018-07-17 16:19:38 -0700

    new pics and initialize level

---
+++ Platformer/level1.swift
@@ -0,0 +1,13 @@
+//
+//  level1.swift
+//  Platformer
+//
+//  Created by Brandon Shien on 7/17/18.
+//  Copyright © 2018 Brandon Shien. All rights reserved.
+//
+
+import UIKit
+
+class level1: SKScene {
+
+}

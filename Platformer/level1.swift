d713cebf92fde8c6d8b1bf369ab268cc1e27a30c
Brandon Shien <brandon@macbook-pro-7.hsd1.ca.comcast.net>
2018-08-01 12:03:31 -0700

    failed hit detection

--- Platformer/level1.swift
+++ Platformer/level1.swift
@@ -9,13 +9,19 @@
 
 import SpriteKit
 import GameplayKit
+import UIKit
 
 
-class level1: SKScene, VaudevilleSpriteNodeButtonDelegate {
+class level1: SKScene, VaudevilleSpriteNodeButtonDelegate, SKPhysicsContactDelegate {
     var jumpButton: VaudevilleSpriteNodeButton!
     var leftButton: VaudevilleSpriteNodeButton!
     var rightButton: VaudevilleSpriteNodeButton!
     var cam: SKCameraNode?
+    let carrotCategory: UInt32 = 0x1 << 0
+    let ghostCategory: UInt32 = 0x1 << 1
+    
+    
+    
     
     override func didMove(to: SKView){
         jumpButton = childNode(withName: "jumpButton") as! VaudevilleSpriteNodeButton
@@ -28,6 +34,7 @@ class level1: SKScene, VaudevilleSpriteNodeButtonDelegate {
         cam = SKCameraNode()
         self.camera = cam
         self.addChild(cam!)
+        self.physicsWorld.contactDelegate = self
         
         
         
@@ -35,7 +42,7 @@ class level1: SKScene, VaudevilleSpriteNodeButtonDelegate {
     
     override func update(_ currentTime: TimeInterval){
         let skele = self.childNode(withName: "skele")
-        let skelePos = skele?.position
+        
     //called before each frame is rendered
         let jumpUp = SKAction.moveBy(x:0, y: 10, duration: 0.2)
        
@@ -53,6 +60,20 @@ class level1: SKScene, VaudevilleSpriteNodeButtonDelegate {
         super.update(currentTime)
         if let camera = cam, let pl = skele {
             camera.position = pl.position
+        }
+        if self.childNode(withName: "ghostNode") == nil {
+            let ghost = SKSpriteNode(imageNamed: "ghost.png")
+            ghost.position = CGPoint(x: size.width, y: (skele?.position.y)!)
+            ghost.name = "ghostNode"
+            ghost.physicsBody = SKPhysicsBody(rectangleOf: ghost.frame.size)
+            ghost.physicsBody?.isDynamic = true
+            ghost.physicsBody?.affectedByGravity = false
+            ghost.physicsBody?.velocity = CGVector(dx: -100, dy:0)
+            
+            ghost.physicsBody?.usesPreciseCollisionDetection = true
+            ghost.physicsBody?.categoryBitMask = ghostCategory
+            ghost.physicsBody?.collisionBitMask = 0
+            self.addChild(ghost)
         }
        
     }
@@ -99,7 +120,24 @@ class level1: SKScene, VaudevilleSpriteNodeButtonDelegate {
     }
     
     
-    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let secondNode = contact.bodyB.node as! SKSpriteNode
+        let firstNode = contact.bodyA.node as! SKSpriteNode
+        
+        if(contact.bodyA.categoryBitMask == carrotCategory) && (contact.bodyB.categoryBitMask == ghostCategory) {
+            let contactPoint = contact.contactPoint
+            let contact_y = contactPoint.y
+            let target_y = secondNode.position.y
+            let margin = secondNode.frame.size.height/2 - 25
+            
+            if(contact_y > (target_y - margin)) && (contact_y < (target_y + margin)) {
+                secondNode.run(SKAction.removeFromParent())
+                firstNode.run(SKAction.removeFromParent())
+                //maybe this part doesn't work
+            }
+        }
+        
+    }
     
     
     
@@ -118,7 +156,9 @@ class level1: SKScene, VaudevilleSpriteNodeButtonDelegate {
         bullet.name = "bulletNode"
         bullet.physicsBody = SKPhysicsBody(rectangleOf: bullet.frame.size)
         bullet.physicsBody?.usesPreciseCollisionDetection = true
-        
+        bullet.physicsBody?.categoryBitMask = carrotCategory
+        bullet.physicsBody?.contactTestBitMask = carrotCategory | ghostCategory
+        bullet.physicsBody?.collisionBitMask = 0
         return bullet
         
     }
